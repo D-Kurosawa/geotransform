bd92dd9bf1eb4f08bf66992e918fb9754df505b8
Daisuke Kurosawa <dai-kuro@sings.jp>
2019-12-06 13:07:26 +0900

    [move] class Epsg ---> class variable in class GeoTrans

--- geotransform/geotrans.py
+++ geotransform/geotrans.py
@@ -4,31 +4,50 @@ from pyproj import Proj
 from pyproj import Transformer
 
 
-class Epsg:
-    wgs84 = 4326  # WGS84: 地理座標系, GPSで得られる位置, 緯度経度
-    jgd2000 = 4612  # JGD2000: 地理座標系, 世界測地系, 経度緯度
-    jgd2011 = 6668  # JGD2011: 地理座標系, 世界測地系, 経度緯度
-    tokyo = 4301  # 日本測地系TOKYO: 地理座標系, 日本測地系, 経度緯度
-
-
 class GeoTrans:
     """
-    :type _from_epsg: Proj
-    :type _to_epsg: Proj
+    :type _from_epsg: int
+    :type _to_epsg: int
     :type _transformer: Transformer
     """
+    _EPSG_CODE = {
+        4326: {'name': 'WGS84', 'type': 'GeodeticCRS', 'area': 'World'},
+        4612: {'name': 'JGD2000', 'type': 'GeodeticCRS', 'area': 'Japan'},
+        6668: {'name': 'JGD2011', 'type': 'GeodeticCRS', 'area': 'Japan'},
+        4301: {'name': 'Tokyo', 'type': 'GeodeticCRS', 'area': 'Japan'}
+    }
 
-    def __init__(self, from_epsg=Epsg.tokyo, to_epsg=Epsg.jgd2011):
+    def __init__(self, from_epsg=4301, to_epsg=6668):
         """
         :type from_epsg: int
         :type to_epsg: int
         :param from_epsg: EPSG code of conversion source
         :param to_epsg: EPSG code of destination
         """
-        self._from_epsg = Proj(f"epsg:{from_epsg}")
-        self._to_epsg = Proj(f"epsg:{to_epsg}")
+        self._from_epsg = from_epsg
+        self._to_epsg = to_epsg
         self._transformer = Transformer.from_proj(
-            self._from_epsg, self._to_epsg, always_xy=True)
+            proj_from=Proj(f"epsg:{from_epsg}"),
+            proj_to=Proj(f"epsg:{to_epsg}"),
+            always_xy=True
+        )
+
+    def epsg_info(self):
+        print(f"EPSG from: {self._from_epsg}"
+              f"    Name: {self._get_epsg_name(self._from_epsg)}")
+        print(f"EPSG to  : {self._to_epsg}"
+              f"    Name: {self._get_epsg_name(self._to_epsg)}")
+
+    @classmethod
+    def _get_epsg_name(cls, code):
+        """
+        :type code: int
+        :rtype str
+        """
+        if code in cls._EPSG_CODE:
+            return cls._EPSG_CODE[code]['name']
+
+        return f"Not expect in <class {cls.__name__}>"
 
     def transform(self, lng, lat):
         """
